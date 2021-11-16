13d27eef0b3f84ebd2b6c497485088e5539318a6
Coen van Gruijthuijsen <coengruijt@gmail.com>
2021-11-16 18:40:31 +0100

    Update TokenTypes

--- src/lexer.rs
+++ src/lexer.rs
@@ -2,6 +2,32 @@ pub fn return_5()-> i32 {
     println!("I am the Lexer");
     5
 }
+
+pub enum TokenType { 
+    OpenBrace,
+    CloseBrace, 
+    OpenParenthesis,
+    CloseParenthesis,
+    SemiColon, 
+    Int,
+    Return,
+    Identifier,
+    IntegerLiteral
+
+}
+
+
+pub struct Token { 
+    token_type: TokenType 
+}
+
+
+pub fn lex_string() -> crate::lexer::Token { 
+
+    return Token { token_type: TokenType::OpenBrace }
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
