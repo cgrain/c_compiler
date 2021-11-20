35e897c9f5e9ecc5f401031f8c36f7c9397f249e
Coen van Gruijthuijsen <coengruijt@gmail.com>
2021-11-20 16:49:29 +0100

    Adding support for brackets

--- src/parser/parser_match_expression.rs
+++ src/parser/parser_match_expression.rs
@@ -6,7 +6,7 @@ pub fn parse_expression(token_list: &[Token]) -> (Option<Node>, &[Token]) {
     return parse_primary_expression(token_list);
 }
 
-pub fn parse_primary_expression(token_list: &[Token]) -> (Option<Node>, &[Token]) {
+fn parse_primary_expression(token_list: &[Token]) -> (Option<Node>, &[Token]) {
     match token_list.get(0) {
         Some( Token { token_type: TokenType::IntegerLiteral, value: v, ..}) => {
             return (Some(Node { kind: NodeKind::Expression, parent:None, children: vec![], value:v.clone() }), &token_list[1..]);
@@ -19,6 +19,32 @@ pub fn parse_primary_expression(token_list: &[Token]) -> (Option<Node>, &[Token]
         }
     }
 } 
+
+fn parse_expression_in_brackets(token_list: &[Token]) -> (Option<Node>, &[Token]) {
+    // <Expression> := OpenParenthesis <Expression> CloseParenthesis
+    match token_list.get(0) {
+        Some( Token { token_type: TokenType::OpenParenthesis, ..}) => {
+            // We know this matches (Otherwise it would not be called), but just to be sure
+            let mut open_parenthesis = 1; 
+            for i in 1..token_list.len() {
+                if token_list[i].token_type == TokenType::CloseParenthesis { 
+                    open_parenthesis -= 1;
+                    if open_parenthesis == 0 {
+                        let (expression, rest) = parse_expression(&token_list[1..i]);
+                        return (expression, &token_list[i+1..]);
+                    }
+                } else if token_list[i].token_type == TokenType::OpenParenthesis {
+                    open_parenthesis += 1;
+                }
+
+            }
+            panic!("Unbalanced Parenthesis!")
+        } 
+        _ => {
+            return (None, token_list);
+        }
+    }
+}
 
 
 
