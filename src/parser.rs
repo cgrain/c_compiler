c1770e6f12466339a72c662937e3daedb78fe1dc
Coen van Gruijthuijsen <coengruijt@gmail.com>
2021-11-21 17:56:38 +0100

    Renaming

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,5 @@
 mod parser_matcher;
 mod parser_match_expression;
-mod parser_match_expression2;
 pub mod parser_nodes;
 use crate::lexer::lexer_types::{Token};
 pub fn parse(token_list: &[Token]) -> Option<parser_nodes::Node> { 
